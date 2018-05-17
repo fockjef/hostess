9a352d4dd702d9344c357e82baf4f4895c223037
jef <fockjef@gmail.com>
2018-05-17 11:31:20 -0500

    Moves config to external json file, build windows executable

--- hostess.pl
+++ hostess.pl
@@ -2,45 +2,28 @@
 
 use strict;
 use Getopt::Long;
+use JSON;
 use List::Util qw(sum);
 use LWP::UserAgent;
 
-my %Config = (
-    custom => [
-        {host=>'localhost',addr=>'127.0.0.1'},
-        {host=>'localhost ip6-localhost ip6-loopback',addr=>'::1'},
-        {host=>'ip6-allnodes',addr=>'ff02::1'},
-        {host=>'ip6-allrouters',addr=>'ff02::2'}
-    ],
-    sources => [
-        {name=>'airel',type=>'mis' ,weight=>0.35,url=>'http://rlwpx.free.fr/WPFF/hmis.7z'},
-        {name=>'airel',type=>'pub' ,weight=>0.35,url=>'http://rlwpx.free.fr/WPFF/hpub.7z'},
-        {name=>'airel',type=>'rsk' ,weight=>0.35,url=>'http://rlwpx.free.fr/WPFF/hrsk.7z'},
-        {name=>'airel',type=>'sex' ,weight=>0.00,url=>'http://rlwpx.free.fr/WPFF/hsex.7z'},
-        {name=>'airel',type=>'trc' ,weight=>0.35,url=>'http://rlwpx.free.fr/WPFF/htrc.7z'},
-        {name=>'camel',type=>''    ,weight=>0.95,url=>'http://sysctl.org/cameleon/hosts'},
-        {name=>'hfnet',type=>'main',weight=>0.35,url=>'http://hosts-file.net/download/hosts.zip'},
-        {name=>'hfnet',type=>'part',weight=>0.35,url=>'http://hosts-file.net/hphosts-partial.txt'},
-        {name=>'hforg',type=>''    ,weight=>0.65,url=>'http://hostsfile.org/Downloads/BadHosts.msw.zip'},
-        {name=>'moaab',type=>''    ,weight=>0.30,url=>'http://adblock.mahakala.is/'},
-        {name=>'mvps' ,type=>''    ,weight=>0.95,url=>'http://winhelp2002.mvps.org/hosts.zip'},
-        {name=>'mware',type=>''    ,weight=>0.95,url=>'http://www.malwaredomainlist.com/hostslist/hosts.txt'},
-        {name=>'s1who',type=>''    ,weight=>0.95,url=>'http://someonewhocares.org/hosts/hosts'}
-    ],
-    whitelist => [qw/localhost local localhost.localdomain android.localhost test.localhost broadcasthost android test/],
-    source_dir  => "sources",
-    block_score => 1.0,
-    block_addr  => "0.0.0.0",
-    hosts_file  => "hosts"
-);
+our $VERSION = "1.0.20180517";
+
+my $NULL = $^O eq "MSWin32" ? "NUL" : "/dev/null";
 
 my %Opts = (
-    download => 1
+	config => 'hostess.conf',
+	download => 1
 );
-GetOptions("download!"=>\$Opts{download});
+GetOptions(
+	"config:s"=>\$Opts{config},
+	"download!"=>\$Opts{download}
+);
 
-if( !-d $Config{source_dir} ){
-    mkdir $Config{source_dir} or die "ERROR: Cannot create directory '$Config{source_dir}'";
+my $json = do{local $/;open my $IN, $Opts{config} or die "ERROR: Cannot open config file ($Opts{config})";<$IN>};
+my $Config = eval { decode_json $json } or die "ERROR: Cannot parse json";
+
+if( !-d $Config->{source_dir} ){
+    mkdir $Config->{source_dir} or die "ERROR: Cannot create directory '$Config->{source_dir}'";
 }
 
 my $ua = LWP::UserAgent->new();
@@ -48,10 +31,10 @@ $ua->agent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Geck
 
 my %Hosts;
 
-foreach my $s (@{$Config{sources}}){
+foreach my $s (@{$Config->{sources}}){
     print STDERR $s->{name}, " ", ($s->{type}?"($s->{type})":""), ":\n";
     my $ext = ($s->{url} =~ /\.([^\/.]+?)$/)[0] || "txt";
-    my $file = $Config{source_dir}."/".$s->{name}.($s->{type}?"-".$s->{type}:"").".".$ext;
+    my $file = $Config->{source_dir}."/".$s->{name}.($s->{type}?"-".$s->{type}:"").".".$ext;
 
     if( $Opts{download} ){
         print STDERR "\tDownloading\t";
@@ -61,7 +44,7 @@ foreach my $s (@{$Config{sources}}){
     
     print STDERR "\tProcessing\t";
     my $n = 0;
-    open my $DATA, $ext eq "txt" ? $file : "7z e -so $file 2>/dev/null |" or die "ERROR: Cannot open file '$file'";
+    open my $DATA, ($ext eq "txt" ? $file : "7z.exe e -so $file 2>$NULL |") or die "ERROR: Cannot open file '$file'";
     foreach my $h (map {(split)[1]} grep {/^\s*(?:127\.0\.0\.1|0\.0\.0\.0)\s+/} <$DATA>){
         if( !exists $Hosts{$h}{$s->{name}} || $Hosts{$h}{$s->{name}} < $s->{weight} ){
             $Hosts{$h}{SCORE} += $s->{weight}-$Hosts{$h}{$s->{name}};
@@ -72,12 +55,12 @@ foreach my $s (@{$Config{sources}}){
     print STDERR "$n hosts\n";
 }
 
-delete @Hosts{@{$Config{whitelist}}};
-my @Blocked = grep { $Hosts{$_}{SCORE} >= $Config{block_score} } keys %Hosts;
+delete @Hosts{@{$Config->{whitelist}}};
+my @Blocked = grep { $Hosts{$_}{SCORE} >= $Config->{block_score} } keys %Hosts;
 print STDERR "Blocked ".scalar(@Blocked)." hosts\n";
 
-open my $OUT, ">$Config{hosts_file}";
-print $OUT join "\n", map { sprintf "%-15s\t%s", $_->{addr}, $_->{host} } @{$Config{custom}};
+open my $OUT, ">$Config->{hosts_file}";
+print $OUT join "\n", map { sprintf "%-15s\t%s", $_->{addr}, $_->{host} } @{$Config->{custom}};
 print $OUT "\n\n";
-print $OUT join "\n", map { sprintf "%-15s\t%s", $Config{block_addr}, $_ } sort @Blocked;
+print $OUT join "\n", map { sprintf "%-15s\t%s", $Config->{block_addr}, $_ } sort @Blocked;
 close $OUT;
